db617300c32343328d332dc324e1893b5939e8f8
Adam Jorgensen <adam.jorgenzen.za@gmail.com>
2017-09-09 09:28:21 +0200

    Added utils module with generate_name function. Switched name generation from using first 8 characters of uuid4 to custom scheme that generates valid Python variable numbers in order to aid usage of attribute access provided by HasNames mixing

---
+++ opendna/autobahn/repl/utils.py
@@ -0,0 +1,45 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2017 OpenDNA Ltd.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+################################################################################
+from uuid import uuid4
+from random import choice, choices
+import string
+
+__author__ = 'Adam Jorgensen <adam.jorgensen.za@gmail.com>'
+
+
+def generate_name(name: str=None, length: int=8):
+    """
+    Generate a name if the input name is None. The generated name will start
+    with an ASCII letter and be followed by length-1 ASCII letters and/or numbers
+
+    :param name:
+    :param length:
+    :return:
+    """
+    if name is None:
+        name = (
+            choice(string.ascii_letters)
+            + ''.join(choices(string.ascii_letters + string.digits, k=length-1))
+        )
+    return name
